f46b06143315da3395f69a6eff047a8babefbf35
Ivan Carcamo <ivandres73@unitec.edu>
2024-06-19 14:27:55 +0000

    security group added

--- main.tf
+++ main.tf
@@ -1,28 +1,60 @@
-provider "aws" {
-  version = "2.33.0"
-
-  region = var.aws_region
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
 }
 
-resource "aws_dynamodb_table" "tfc_example_table" {
-  name = var.db_table_name
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
 
-  read_capacity  = var.db_read_capacity
-  write_capacity = var.db_write_capacity
-  hash_key       = "UUID"
-  range_key      = "UserName"
+data "aws_vpc" "main" {
+  id = "vpc-106b3a6a"
+}
 
-  attribute {
-    name = "UUID"
-    type = "S"
+resource "aws_security_group" "example" {
+  # ... other configuration ...
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = data.aws_vpc.main.id
+
+
+  dynamic "ingress" {
+    for_each = local.ingress
+    content {
+      from_port   = ingress.value.port
+      to_port     = ingress.value.port
+      description = ingress.value.description
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
   }
 
-  attribute {
-    name = "UserName"
-    type = "S"
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    description      = "outgoing for everyone"
+    prefix_list_ids  = []
+    self             = false
+    security_groups  = []
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
+}
 
-  tags = {
-    user_name = var.tag_user_name
-  }
+locals {
+  ingress = [
+    {
+      port        = 443
+      description = "port 443"
+    },
+    {
+      port        = 80
+      description = "port 80"
+    }
+  ]
 }
